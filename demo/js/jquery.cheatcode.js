7adb6be5f062ef7d2756daed872a6e9a4fdcc2ab
Kevin Beronilla <kevinberonilla@Kevins-MacBook-Pro.local>
2015-05-21 07:28:45 -0500

    Initial commit for demo

---
+++ demo/js/jquery.cheatcode.js
@@ -0,0 +1,24 @@
+(function ($) {
+	$.fn.cheatcode = function(options) {
+		var timer,
+		input = '',
+		settings = $.extend({
+			code: '38384040373937396665',
+			time: 500,
+			callback: function() { alert('Cheat activated!'); }
+		}, options);
+		
+		function checkInput() {
+			if (input == settings.code) {
+				settings.callback.call(this);
+			}
+		}
+		
+		this.keyup(function(e) {
+			input += e.which;
+			clearTimeout(timer);
+			timer = setTimeout(function() { input = ""; }, settings.time);
+			checkInput();
+		});
+	}
+}(jQuery));
