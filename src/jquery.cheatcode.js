5193b932bf9e0f2f1df06f6aef09921aa37d3ba4
Kevin Beronilla <kevin.beronilla@gmail.com>
2015-06-02 20:23:13 -0500

    Added comments

--- src/jquery.cheatcode.js
+++ src/jquery.cheatcode.js
@@ -7,11 +7,11 @@ http://www.kevinberonilla.com
 Fork on GitHub
 https://github.com/kevinberonilla/jquery-cheatcode
 -------------------------------------------------- */
-(function ($) {
+(function ($) { // Protect the $ alias (IIF)
 	$.fn.cheatcode = function(options) {
 		var timer,
 		input = '',
-		settings = $.extend({
+		settings = $.extend({ // Extend the default settings
 			code: '38384040373937396665',
 			time: 500,
 			callback: function() { alert('Invincibility enabled!'); }
@@ -19,17 +19,17 @@ https://github.com/kevinberonilla/jquery-cheatcode
 		
 		function checkInput() {
 			if (input == settings.code) {
-				settings.callback.call(this);
+				settings.callback.call(this); // Call the callback
 			}
 		}
 		
 		this.keyup(function(e) {
-			input += e.which;
+			input += e.which; // Add the pressed key code
 			clearTimeout(timer);
-			timer = setTimeout(function() { input = ''; }, settings.time);
+			timer = setTimeout(function() { input = ''; }, settings.time); // Clear the input if time runs out
 			checkInput();
 		});
 		
-		return this;
+		return this; // Return the object to enable chaining
 	}
 }(jQuery));
